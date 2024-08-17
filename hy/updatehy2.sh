e53d3940e6a0518211ee318bf9f317d2adf2f4a7
frankiejun <frankiejun@qq.com>
2024-08-17 22:44:22 +0800

    fixed hyupdate

--- hy/updatehy2.sh
+++ hy/updatehy2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  
+
 # 定义变量
 BINARY_PATH="/usr/local/bin/hysteria"
 BACKUP_PATH="/usr/local/bin"
@@ -12,32 +12,31 @@ echo "UPDATE_INFO:$UPDATE_INFO"
 # 解析更新信息
 UPDATE_AVAILABLE=$(echo "$UPDATE_INFO" | grep "no update available")
 echo "UPDATE_AVAILABLE:$UPDATE_AVAILABLE"
-if [ -n "UPDATE_AVAILABLE" ]; then
+if [ -n "$UPDATE_AVAILABLE" ]; then
   echo "没有更新可用"
   exit
 fi
 
 UPDATE_AVAILABLE=$(echo "$UPDATE_INFO" | grep "update available")
 if [ -n "$UPDATE_AVAILABLE" ]; then
-    exit;
-    VERSION=$(echo "$UPDATE_AVAILABLE" | grep -oP '(?<=version": ")[^"]+')
-    URL="https://github.com/apernet/hysteria/releases/download/app%2F${VERSION}/hysteria-linux-amd64"
+  VERSION=$(echo "$UPDATE_AVAILABLE" | grep -oP '(?<=version": ")[^"]+')
+  URL="https://github.com/apernet/hysteria/releases/download/app%2F${VERSION}/hysteria-linux-amd64"
 
-    # 下载最新版本
-    wget -O hysteria-new "$URL"
+  # 下载最新版本
+  wget -O hysteria-new "$URL"
 
-    # 检查是否存在旧版本，备份旧版本
-    if [ -f "$BINARY_PATH" ]; then
-	    systemctl stop hysteria-server.service
-	    mv "$BINARY_PATH" "${BACKUP_PATH}/hysteria.${DATE}"
-    fi
+  # 检查是否存在旧版本，备份旧版本
+  if [ -f "$BINARY_PATH" ]; then
+    systemctl stop hysteria-server.service
+    mv "$BINARY_PATH" "${BACKUP_PATH}/hysteria.${DATE}"
+  fi
 
-    # 替换为最新版本并重启服务
-    mv hysteria-new "$BINARY_PATH"
-    chmod +x "$BINARY_PATH"
-    systemctl start hysteria-server.service
+  # 替换为最新版本并重启服务
+  mv hysteria-new "$BINARY_PATH"
+  chmod +x "$BINARY_PATH"
+  systemctl start hysteria-server.service
 
-    echo "Hysteria has been updated to version $VERSION."
+  echo "Hysteria has been updated to version $VERSION."
 else
-    echo "No updates available."
+  echo "No updates available."
 fi
